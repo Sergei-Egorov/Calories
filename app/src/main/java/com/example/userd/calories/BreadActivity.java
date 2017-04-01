3cdee2609b2a57789d07314681239c782e0613bc
Sergei-Egorov <wwhush@gmail.com>
2017-03-31 20:37:34 -0400

    v. 0.1.2 30.03.2017

---
+++ app/src/main/java/com/example/userd/calories/BreadActivity.java
@@ -0,0 +1,13 @@
+package com.example.userd.calories;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BreadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bread);
+    }
+}
--- app/src/main/java/com/example/userd/calories/Food.java
+++ app/src/main/java/com/example/userd/calories/Food.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Food extends AppCompatActivity {
 
@@ -11,7 +12,50 @@ public class Food extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
-    }
 
 
+        final TextView FruitView = (TextView) findViewById(R.id.FruitView);
+        final TextView VegetablesView = (TextView) findViewById(R.id.VegetablesView);
+        final TextView PastaView = (TextView) findViewById(R.id.PastaView);
+        final TextView BreadView = (TextView) findViewById(R.id.BreadView);
+        final TextView MeatView = (TextView) findViewById(R.id.MeatView);
+
+
+        MeatView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent MeatIntent = new Intent(Food.this, MeatActivity.class);
+            Food.this.startActivity(MeatIntent);
+        }
+    });
+        FruitView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent FruitIntent = new Intent(Food.this, FruitActivity.class);
+                Food.this.startActivity(FruitIntent);
+            }
+        });
+        VegetablesView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent VegetablesIntent = new Intent(Food.this, VegetablesActivity.class);
+                Food.this.startActivity(VegetablesIntent);
+            }
+        });
+        PastaView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent PastaIntent = new Intent(Food.this, PastaActivity.class);
+                Food.this.startActivity(PastaIntent);
+            }
+        });
+        BreadView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent BreadIntent = new Intent(Food.this, BreadActivity.class);
+                Food.this.startActivity(BreadIntent);
+            }
+        });
 }
+}
+
